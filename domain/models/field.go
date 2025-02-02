e1d0a17fda44cbeaee7f56ec4d4b368295b87172
Rizki pandiwa <pandiwarizki.rp@gmail.com>
2025-02-02 12:10:22 +0700

    setup model&dto 1

---
+++ domain/models/field.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Field struct {
+	ID             uint      `gorm:"primaryKey,autoIncrement"`
+	UUID           uuid.UUID `gorm:"type:uuid;default:not null;unique"`
+	Code           string    `gorm:"type:varchar(15);not null"`
+	Name           string    `gorm:"type:varchar(50);not null"`
+	pricePerHour   int       `gorm:"type:int;not null"`
+	Images         string    `gorm:"type:text[];not null"`
+	CreatedAt      *time.Time
+	UpdatedAt      *time.Time
+	DeletedAt      *time.Time
+	FieldSchedules []FieldSchedule `gorm:"foreignKey:FieldID; references:ID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
---
+++ domain/models/field_schedule.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+
+	"field-service/constants"
+)
+
+type FieldSchedule struct {
+	ID        uint                          `gorm:"primaryKey,autoIncrement"`
+	UUID      uuid.UUID                     `gorm:"type:uuid;default:not null;unique"`
+	FieldID   uint                          `gorm:"type:int;not null"`
+	timeID    uint                          `gorm:"type:int;not null"`
+	Date      *time.Time                    `gorm:"type:date;not null"`
+	Status    constants.FieldScheduleStatus `gorm:"type:int;not null"`
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+	DeletedAt *time.Time
+	Field     Field `gorm:"foreignKey:FieldID; references:ID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Time      Time  `gorm:"foreignKey:timeID; references:ID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
